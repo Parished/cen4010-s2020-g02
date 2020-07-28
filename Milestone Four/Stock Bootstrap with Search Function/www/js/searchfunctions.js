f3bca0096df62201a28798b011954748bc34d43e
Parished <Peradox@live.com>
2020-07-27 21:34:13 -0400

    Search Function
    This bootstrap template is an older one that had very little edits.

---
+++ Milestone Four/Stock Bootstrap with Search Function/www/js/searchfunctions.js
@@ -0,0 +1,43 @@
+const search_movie = function(){
+    document.getElementById('search_button').addEventListener('click', sendMovie);
+}
+
+const sendMovie = function(event){
+    event.preventDefault();
+    const user_title = document.getElementById('title').value;
+    localStorage.setItem('title', JSON.stringify(user_title));
+    document.getElementById('form-search').submit();
+}
+
+function consoleTitle(){
+    console.log('Hello, Console!')
+}
+
+function apiCall(){
+    let movie_name_storage = localStorage.getItem('title');
+    $.getJSON('http://www.omdbapi.com/?t=' + movie_name_storage + encodeURI('&apikey=7b3941a8') + "&plot=full").then(function(response){
+    console.log(response);
+    let api_response_print = response;
+    
+    document.querySelector('#details_title').innerHTML = api_response_print.Title;
+    document.querySelector('#director_name').innerHTML = api_response_print.Director;
+    document.querySelector('#cast_names').innerHTML = api_response_print.Actors;
+    document.querySelector('#genre_details').innerHTML = api_response_print.Genre;
+    document.querySelector('#release_year').innerHTML = api_response_print.Year;  
+    document.querySelector('#runtime_details').innerHTML = api_response_print.Runtime;
+    document.querySelector('#country_details').innerHTML = api_response_print.Country;
+    document.querySelector('#plot_details').innerHTML = api_response_print.Plot;
+    document.querySelector('#poster_details').src = api_response_print.Poster;
+    
+    
+
+
+   });
+}
+
+document.addEventListener('DOMContentLoaded', search_movie);
+
+function modify_details_page(){
+    console.log("loading modify details script");
+    apiCall();
+}
